8712f28a62562dbb46e6346c2e3b4c73f90122ad
ShawonECE <send2shawon@gmail.com>
2024-06-10 21:42:41 +0600

    framer motion used in package card

--- src/components/PackageCard.jsx
+++ src/components/PackageCard.jsx
@@ -4,14 +4,17 @@ import Button from './Button';
 import { useNavigate } from 'react-router-dom';
 import useAxiosPublic from '../hooks/useAxiosPublic';
 import { AuthContext } from './AuthProvider';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import swal from 'sweetalert';
+import { motion, useInView } from 'framer-motion';
 
 const PackageCard = ({tourPackage}) => {
     const { coverImage, tourType, tripTitle, price, _id } = tourPackage;
     const { user } = useContext(AuthContext);
     const navigate = useNavigate();
     const axiosPublic = useAxiosPublic();
+    const ref = useRef(null)
+    const inView = useInView(ref);
     const handleAddWishlist = () => {
         if (!user) {
             swal("You have to log in to add wishlist!", {
@@ -39,22 +42,28 @@ const PackageCard = ({tourPackage}) => {
         }
     };
     return (
-        <div className="card bg-[#F2613F] bg-opacity-10 shadow-2xl">
+        <motion.div
+            ref={ref}
+            className="card bg-[#F2613F] bg-opacity-10 shadow-2xl"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+            transition={{ duration: 0.5 }}
+        >
             <figure><img src={coverImage} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">
-                    { tripTitle }
+                    {tripTitle}
                 </h2>
-                <div className="badge badge-outline border-[#F2613F] text-[#F2613F]">{ tourType } Tour</div>
+                <div className="badge badge-outline border-[#F2613F] text-[#F2613F]">{tourType} Tour</div>
                 <div className="flex justify-around">
                     <p>Price: <span className='font-semibold'>${price}</span></p>
-                    <CiHeart onClick={handleAddWishlist} className='text-xl cursor-pointer'/>
+                    <CiHeart onClick={handleAddWishlist} className='text-xl cursor-pointer' />
                 </div>
                 <div className="card-actions justify-end mt-2">
                     <Button text='View Details' click={() => navigate(`/package/${_id}`)}></Button>
                 </div>
             </div>
-        </div>
+        </motion.div>
     );
 };
 
