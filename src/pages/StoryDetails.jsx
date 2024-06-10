ac84424811f745c2ff66e964ea7a0dd2bac0751e
ShawonECE <send2shawon@gmail.com>
2024-06-10 11:03:16 +0600

    Sharing story to social media feature implemented

--- src/pages/StoryDetails.jsx
+++ src/pages/StoryDetails.jsx
@@ -2,10 +2,12 @@ import { useParams } from "react-router-dom";
 import useAxiosPublic from "../hooks/useAxiosPublic";
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
+import { FacebookIcon, FacebookShareButton, LinkedinIcon, LinkedinShareButton, WhatsappIcon, WhatsappShareButton } from "react-share";
 
 const StoryDetails = () => {
     const { id } = useParams();
     const axiosPublic = useAxiosPublic();
+    const shareUrl = `https://compass-55799.web.app/story/${id}`;
 
     const { isPending, data = {} } = useQuery({ queryKey: [`story_${id}`], queryFn: async() => {
         const data = await axiosPublic.get(`/story/${id}`);
@@ -29,8 +31,25 @@ const StoryDetails = () => {
                 <title>Story Details</title>
             </Helmet>
             <div className="flex justify-between">
-                <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold">{touristName} was in {tourLocation}</h1>
-                <h1 className="text-xl md:text-2xl lg:text-3xl font-bold">By: <span className="text-[#F2613F]">{touristName}</span></h1>
+                <div>
+                    <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold mb-2">{touristName} was in {tourLocation}</h1>
+                    <h1 className="text-lg md:text-xl font-bold">By: <span className="text-[#F2613F]">{touristName}</span></h1>
+                </div>
+                <div className="flex items center gap-2">
+                    <FacebookShareButton url={shareUrl}>
+                        <FacebookIcon size={32} round />
+                    </FacebookShareButton>
+                    <LinkedinShareButton url={shareUrl}>
+                        <LinkedinIcon size={32} round />
+                    </LinkedinShareButton>
+                    <WhatsappShareButton
+                        url={shareUrl}
+                        title="Story from compass travels"
+                        separator=":: "
+                    >
+                        <WhatsappIcon size={32} round />
+                    </WhatsappShareButton>
+                </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-8">
                 <img src={image1} alt="" className="w-full rounded-md" />
